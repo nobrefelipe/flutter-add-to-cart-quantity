401b97320fb9fa87eef43843db73701c01190631
Felipe Nobre <nobrefelipe1@gmail.com>
2020-06-01 21:19:05 +0100

    commented the usage of canvasColor

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,15 @@ import 'add-to-cart-widget/add_to_cart_widget.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    /*
+     * Includes the AddToCart Widget
+     * use Theme widget so we can set the canvasColor to trasnparent
+     * this allow us to have rounded borders on the top of the Bottom Sheet
+     * we could set it on the global app theme
+     * but this way we only change the canvas color of the widget and not the entire app
+     * 
+     */
     return Theme(
       data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
       child: AddToCart(),
